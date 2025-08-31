272c14982511e661e290f48fb81f69d143d7ddb8
nguyen trung duc <trunghien1175@gmail.com>
2025-08-31 16:23:59 +0700

    Them ham addTree vao Bai12.cpp

--- Bai12.cpp
+++ Bai12.cpp
@@ -3,22 +3,22 @@
 struct Node
 {
     int data;
-    Node* pLeft;
-    Node* pRight;
+    Node *pLeft;
+    Node *pRight;
 };
 
-Node* initNode(int value)
+Node *initNode(int value)
 {
-    Node* p = new Node;
+    Node *p = new Node;
     p->data = value;
     p->pLeft = nullptr;
     p->pRight = nullptr;
-    return p; 
+    return p;
 }
 
 struct Tree
 {
-    Node* pRoot;
+    Node *pRoot;
 };
 
 void initTree(Tree &t)
@@ -26,18 +26,33 @@ void initTree(Tree &t)
     t.pRoot = nullptr;
 }
 
-void addTree(Tree &t, Node* p)
+void addTree(Tree &t, Node *p)
 {
     if (t.pRoot == nullptr)
     {
         t.pRoot = p;
+        return;
+    }
+    else
+    {
+        Node *pGoto = t.pRoot;
+        Node *pLocal = nullptr;
+        while (pGoto != nullptr)
+        {
+            pLocal = pGoto;
+            if (p->data < pGoto->data)
+                pGoto = pGoto->pLeft;
+            else if (p->data > pGoto->data)
+                pGoto = pGoto->pRight;
+        }
+        if (p->data < pLocal->data)
+            pLocal->pLeft = p;
+        else if (p->data > pLocal->data)
+            pLocal->pRight = p;
     }
 }
-
 
 int main()
 {
     Tree t;
-
 }
-
